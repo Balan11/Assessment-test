4802d022c576543f21d31e58043de9c6a846b4d5
unknown <softbala92@gmail.com>
2024-12-13 08:14:56 +0530

    final

--- Billing_System/billingApp/admin.py
+++ Billing_System/billingApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-from billingApp.models import Product
+from billingApp.models import Product,Denomination,BillDetails,BillInfo
 admin.site.register(Product)
+admin.site.register(BillInfo)
+admin.site.register(BillDetails)
+admin.site.register(Denomination)
--- Billing_System/billingApp/views.py
+++ Billing_System/billingApp/views.py
@@ -5,16 +5,33 @@ from billingApp.forms import  CustomerForms,DenominationForms,BillItemFormSet
 def generateBill(request):
     customform =CustomerForms()
     denominationForms = DenominationForms()
+   
+    if request.method =="POST":
+        customform =CustomerForms(request.POST)
+        denominationForms = DenominationForms(request.POST)
+        billdetails=BillItemFormSet(request.POST, queryset=BillDetails.objects.none())
+        if  customform.is_valid() and billdetails.is_valid() and denominationForms.is_valid():
+            billid=customform.save()
+            for product in billdetails:
+                if product.cleaned_data:
+                    newItem = product.save(commit=False)
+                    newItem.billno = billid
+                    newItem.save()
+            denomination=denominationForms.save(commit=False)
+            denomination.billno=billid
+            denomination.save()
+        return redirect("generateBill")    
     content={
         'customform':customform,
         'denominationForms':denominationForms,
-        'BillItemFormSet':BillItemFormSet()
+        'BillItemFormSet':BillItemFormSet(queryset=BillDetails.objects.none())
     }
-    if request.method =="POST":
-        return redirect("billingpage",id)    
     return render(request,"generateInvoice.html",content)
 def billingpage(request,id):
     bill=BillInfo.objects.get(id=id)
+    cust= BillInfo.objects.get(id=id)
     billinfo = BillDetails.objects.get(billno=bill)
-    content={}
+    content={"billinfo":billinfo}
+    print(content.get("billinfo").billno.customer_email)
     return render(request,"invoice.html",content)
+3.                                  
